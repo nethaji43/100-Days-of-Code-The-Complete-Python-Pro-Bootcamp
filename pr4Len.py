07bd96e34c9ba5bd2f2d8d03ba8a5b7ada865f08
nethaji43 <147912353+nethaji43@users.noreply.github.com>
2024-11-28 11:28:17 +0530

    Add files via upload

---
+++ pr4Len.py
@@ -0,0 +1,1 @@
+print(len(input("enter your name?")))
---
+++ project1.py
@@ -0,0 +1,5 @@
+#this project is generating band name
+print("Welcome to the Band Name Generator.")
+area=input("what's the name of the city you grew up in?")
+pet_name=input("What's your pet name?")
+print("your band name could be " + area+" "+pet_name)
